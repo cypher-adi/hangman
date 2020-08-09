1636d4e777914dbbf3b5ebc00e76fea7bcd35f9b
Cypher <adityasaha444@gmail.com>
2020-08-09 19:56:03 +0530

    wrong letter & word component added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from './components/Header';
 import Figure from './components/Figure';
+import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
 import './App.css';
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Header />
       <div className='game-container'>
         <Figure />
+        <WrongLetters />
+        <Word />
       </div>
     </>
   );
---
+++ src/components/Word.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Word = () => {
+  return <div class='word' id='word'></div>;
+};
+
+export default Word;
