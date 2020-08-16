3fe109b6e2a7cdd9decbdcedf25546c3fa563f04
Cypher <adityasaha444@gmail.com>
2020-08-17 01:02:26 +0530

    parts display on wrong letter

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
     <>
       <Header />
       <div className='game-container'>
-        <Figure />
+        <Figure wrongLetters={wrongLetters} />
         <WrongLetters wrongLetters={wrongLetters} />
         <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
