82c16504c00f3c4f76d4b85026dcea43aa5dd3be
Cypher <adityasaha444@gmail.com>
2020-08-17 10:46:53 +0530

    Updates

---
+++ precache-manifest.4f153eb5da24ccb39a4b4db850b3edf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6849e9b062c1a61ba01a4396bf7169ef",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "2aaae18afd398491d849",
+    "url": "/hangman/static/css/main.e719ed61.chunk.css"
+  },
+  {
+    "revision": "973e31a4495480d1933b",
+    "url": "/hangman/static/js/2.5da62bfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman/static/js/2.5da62bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aaae18afd398491d849",
+    "url": "/hangman/static/js/main.53d34507.chunk.js"
+  },
+  {
+    "revision": "7ef95293683d0ed87720",
+    "url": "/hangman/static/js/runtime-main.6b115b9b.js"
+  },
+  {
+    "revision": "917f5b244b33aeed0a85fc28c564d735",
+    "url": "/hangman/static/media/bg.917f5b24.jpg"
+  }
+]);
